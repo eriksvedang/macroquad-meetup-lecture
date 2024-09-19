479e1cdf118d5ef2e94926033aa45e5875141e68
Erik Svedäng <erik.svedang@gmail.com>
2024-09-19 13:44:19 +0200

    Very first steps of the lecture.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Lecture")]
+async fn main() {
+    let player = Player {
+        position: vec2(screen_width() * 0.5, screen_height() * 0.5),
+        hp: 100,
+    };
+
+    loop {
+        clear_background(WHITE);
+        draw_circle(
+            player.position.x,
+            player.position.y,
+            player.hp as f32 * 0.2,
+            BLACK,
+        );
+        next_frame().await;
+    }
+}
+
+struct Player {
+    position: Vec2,
+    hp: u32,
 }
