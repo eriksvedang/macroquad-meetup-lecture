8f9aabaa2eb265e7cb17fd2b70af09888d43e906
Erik Svedäng <erik.svedang@gmail.com>
2024-09-23 21:05:46 +0200

    Bullets.

---
+++ src/bullet.rs
@@ -0,0 +1,7 @@
+use macroquad::prelude::*;
+
+pub struct Bullet {
+    pub pos: Vec2,
+    pub dir: f32,
+    pub speed: f32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use macroquad::prelude::*;
 
+mod bullet;
 mod player;
+
+use bullet::Bullet;
 
 #[macroquad::main("Lecture")]
 async fn main() {
@@ -10,9 +13,27 @@ async fn main() {
         hp: 100,
     };
 
+    let mut bullets: Vec<Bullet> = Vec::new();
+
     loop {
         clear_background(WHITE);
         player.update(get_input(), get_frame_time());
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            bullets.push(Bullet {
+                pos: player.position,
+                dir: 0.0,
+                speed: 200.0,
+            })
+        }
+
+        for bullet in &mut bullets {
+            bullet.pos += Vec2::from_angle(bullet.dir) * get_frame_time() * bullet.speed;
+            draw_circle(bullet.pos.x, bullet.pos.y, 10.0, RED);
+        }
+
+        bullets.retain(|bullet| bullet.pos.x < screen_width() - 50.0);
+
         player.draw();
         next_frame().await;
     }
